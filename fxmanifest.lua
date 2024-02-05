64e449cf2c41fae5c760ac93d8f7ba2510314268
BxRayven <110394714+BxRayven@users.noreply.github.com>
2024-02-05 12:15:55 +0000

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,9 @@
 game 'gta5'
 fx_version 'adamant'
 
+fivem_checker 'yes'
+github 'https://github.com/BxRayven/name_format_script'
+name '^2Name Format Script'
+version '1.0'
+
 server_script 'server.lua'
