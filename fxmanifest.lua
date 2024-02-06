6b7d2c23355e826414835d9719a1beb81d300bd0
BxRayven <110394714+BxRayven@users.noreply.github.com>
2024-02-06 16:32:54 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,6 @@ fx_version 'adamant'
 fivem_checker 'yes'
 github 'https://github.com/BxRayven/name_format_script'
 name '^2Name Format Script'
-version '1.1'
+version '1.2'
 
 server_script 'server.lua'
